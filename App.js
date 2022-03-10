27b6b7a3daabde11888433635ee65c10f888b3d5
Alex Sylvain Luenga <38809975+0788611515@users.noreply.github.com>
2022-03-10 17:37:10 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from'./logo.svg';
+import '.App.css';
+import PostList from './API/PostList';
+
+function App() {
+    return(
+        <div classNmae="App">
+            <PostList></PostList>
+        </div>
+    );
+}
+
+export default App;
---
+++ PostList.js
@@ -0,0 +1,41 @@
+import Recat, { component } from 'react'
+import axios from 'axios'
+
+class postList extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            post: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.setState({
+                posts: response.data
+            })
+            console.log(response.data)
+        })
+    }
+
+    render() {
+        const {post} = this.state
+        return(
+            <div>
+                <h1>List Of Posts</h1>
+                {
+                 posts.map(post => <div key={posts.id}>{post.title}</div>)
+
+                }
+            </div>
+        )
+        
+    }
+
+}
+
+export default postList
+
